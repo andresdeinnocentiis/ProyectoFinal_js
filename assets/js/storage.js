79de74bf06cd75e61ac3ef449e56ae6eee1d00d5
andresdeinnocentiis <andresdeinnocentiis@gmail.com>
2021-11-30 22:26:41 -0300

    Entrega Final

---
+++ assets/js/storage.js
@@ -0,0 +1,68 @@
+class Storage {
+    static saveProducts(products) {
+        localStorage.setItem("products",JSON.stringify(products))
+    };
+
+    static getAllProducts() {
+        localStorage.getItem('products') ? JSON.parse(localStorage.getItem('products')) : []
+    }
+
+    static getProduct(id) {
+        let products = JSON.parse(localStorage.getItem('cart'));
+        return products.find(product => product.id === id)
+    };
+
+
+    static saveCart(cart) {
+        localStorage.setItem('cart', JSON.stringify(cart));
+    };
+
+    static getCart() {
+        return localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : []
+    };
+
+    static saveTotal(total) {
+        localStorage.setItem("total", JSON.stringify(total))
+    };
+
+    static getTotal() {
+        return localStorage.getItem('total') ? JSON.parse(Number(localStorage.getItem('total'))) : 0;
+    };
+
+    static removeProduct(item) {
+        let cart = this.getCart()
+        cart = cart.filter(product => product.id !== item.id)
+        this.saveCart(cart)
+        
+    }
+
+    static updateAmountProd(item) {
+        let cart = this.getCart()
+        for (let prod in cart) {
+            if (cart[prod].id == item.id) {
+                console.log("PROD AMOUNT: ", Number(cart[prod].amount))
+                console.log("ITEM AMOUNT: ", Number(item.amount))
+                cart[prod].amount += item.amount
+            }
+        }
+        
+        this.saveCart(cart)
+    }
+
+    static updateProduct(item) {
+        let cart = this.getCart()
+
+        for (let prod in cart) {
+            if (cart[prod].id == item.id) {
+                cart[prod] = item
+            }
+        }
+       
+        this.saveCart(cart)
+        
+    }
+}
+
+export {
+    Storage,
+}
